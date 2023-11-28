afcc4d35ad59614be76e7597acff8ee975b77994
jeff-butler-dev <130912131+jeff-butler-dev@users.noreply.github.com>
2023-11-28 16:25:41 -0600

    init setup and extremely basic socket running

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const PORT = process.env.PORT;
+const express = require('express')
+const { createServer } = require('node:http')
+const { join } = require('node:path')
+const {Server} = require('socket.io')
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'))
+})
+
+io.on('connection', (socket) => {
+    console.log('User conncted');
+    socket.on('disconnect', () =>{
+        console.log('user disconnected');
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});
